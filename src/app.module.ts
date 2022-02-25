2573aa364a81a0f3fe978c0d3819b156a9eeb043
DESKTOP-MO9U35H\baki-gul <baki.gul@armagroupholding.com>
2022-02-25 16:19:42 +0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+// import { AuthModule } from './auth/auth.module';
+import { User } from './users/entities/user.entity';
+import { AuthModule } from './auth/auth.module';
+
+const entities = [User];
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: entities,
+      synchronize: true,
+    }),
+    UsersModule,
+    AuthModule,
+    // AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
